4d37dcfb69b691f1e01462dc5d0cdf7c0f5ed478
Mahbub Hasan <mahbubhasan@Mahbubs-MacBook-Pro.local>
2025-03-12 01:24:36 +0600

    change class name

--- ViT_ImageSegmentation/test.py
+++ ViT_ImageSegmentation/test.py
@@ -1,6 +1,6 @@
 import torch
 from project_configuration import ProjectConfiguration
-from encoder.image_econder import ImageEncoder
+from encoder.image_encoder import ImageEncoder
 if __name__ == '__main__':
     config = ProjectConfiguration()
     img = torch.randn(size=(config.batch_size, config.image_channel, config.image_size, config.image_size))
